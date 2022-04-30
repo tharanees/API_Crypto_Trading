a5e1e9f2565e487852bcfd8fd1c854a8f6d3efa2
tharanees <100167588+tharanees@users.noreply.github.com>
2022-04-30 09:44:22 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,49 @@
+##############################################################################################################
+# calculates the profit on each trade after all sell orders filled
+
+from binance.client import Client
+from binance.enums import *
+from binance.exceptions import BinanceAPIException, BinanceOrderException
+import csv
+import pandas as pd
+import math
+from datetime import datetime,timedelta
+
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
+
+api_key = ""
+api_secret = ""
+client = Client(api_key, api_secret)
+
+file = open("data.csv")
+reader = csv.reader(file)
+df = pd.read_csv("data.csv")
+n= len(list(reader))-1
+
+d0 = datetime(2021, 12, 4,0,0,0,0) #enter london date time when the first coin from list was sold
+d1 = datetime.now()
+
+file = open("data.csv")
+reader = csv.reader(file)
+df = pd.read_csv("data.csv")
+n= len(list(reader))-1
+
+for r in range (n):
+    coin = str(df.loc[r, 'COIN'])
+    trades = client.get_my_trades(symbol=coin)
+    tot = 0
+    for t in trades:
+        dt_object = datetime.fromtimestamp((t["time"]/1000))
+        if dt_object >= d0 and dt_object <= d1 and t["isBuyer"] == False:
+            tot = tot + ((float(t["quoteQty"])) - (float(t["commission"])))
+            df.loc[r, 'sold_date'] = dt_object+timedelta(hours=5, minutes=30)
+            df.to_csv("data.csv", index=False)
+    df.loc[r, 'sold_for'] = tot
+    df.loc[r, 'profit'] = df.loc[r, 'sold_for'] - df.loc[r, 'bought_for']
+    df.to_csv("data.csv", index=False)
