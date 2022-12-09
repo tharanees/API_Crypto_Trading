a26eba94b2eb3ce0acc7fb64d8feba6688df2c33
tharaneesjasotharan <100167588+tharanees@users.noreply.github.com>
2022-12-09 15:56:42 +0000

    Update sell.py

--- sell.py
+++ sell.py
@@ -1,5 +1,4 @@
-##############################################################################################################
-#after all buy orders filled, place sell order
+#Run this once all buy orders are filled. It places limit sell orders for the cryptos purchased with a pre-set profit percentage.
 
 from binance.client import Client
 from binance.enums import *
@@ -17,8 +16,8 @@ def round_down(n, decimals=0):
     multiplier = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
 
-api_key = ""
-api_secret = ""
+api_key = "FILL IN YOUR API KEY"
+api_secret = "FILL IN YOUR API SECRET CODE"
 client = Client(api_key, api_secret)
 
 file = open("data.csv")
@@ -26,7 +25,8 @@ reader = csv.reader(file)
 df = pd.read_csv("data.csv")
 n= len(list(reader))-1
 
-d0 = datetime(2021,12,4,0,0,0,0) #enter london date time when the first coin from list was bought
+# datetime(year, month, day, hour, minute, second, microsecond)
+d0 = datetime("FILL IN GMT DATETIME WHEN THE FIRST CRYPTO BUY LIMIT WAS FILLED") 
 d1 = datetime.now()
 
 for r in range (n):
@@ -48,17 +48,16 @@ for r in range (n):
 for r in range (n):
     coin = str(df.loc[r, 'COIN'])
     sell_qty = round_down(df.loc[r, 'QTY'],int(df.loc[r, 'DP2']))
-    # sell_qty = round_down((0.5*(df.loc[r, 'QTY'])),int(df.loc[r, 'DP2']))
     df.loc[r, 'QTY'] = sell_qty
     df.to_csv("data.csv", index=False)
 
-
     qty = round((df.loc[r, 'QTY']),int(df.loc[r, 'DP2']))
-    #placing sell order for all coins bought with a margin of 3.5%
+    #placing sell order for all coins bought with a profit margin of 3.5%
     df.loc[r, 'SOLD_RATE'] = round_up((1.035*(df.loc[r, 'BUY_RATE'])),int(df.loc[r, 'DP1']))
     df.loc[r, 'REMAINING'] = df.loc[r, 'bought_for'] - (df.loc[r, 'QTY']*(df.loc[r, 'BUY_RATE']))
     df.to_csv("data.csv", index=False)
     sell_rate = df.loc[r, 'SOLD_RATE']
+    #place sell limit order
     sell_limit  = client.order_limit_sell(symbol=coin, quantity=qty, price=sell_rate)
 
 
