696ad6d914103730782a86f89fea2a14be636b45
tharaneesjasotharan <100167588+tharanees@users.noreply.github.com>
2022-12-09 15:37:55 +0000

    Update buy.py

--- buy.py
+++ buy.py
@@ -10,8 +10,9 @@ def round_up(n, decimals=0):
     multiplier = 10 ** decimals
     return math.ceil(n * multiplier) / multiplier
 
-api_key = ""
-api_secret = ""
+api_key = "FILL IN YOU API KEY HERE"
+api_secret = "FILL IN YOUR API SECRET CODE HERE"
+#connects to binance server
 client = Client(api_key, api_secret)
 
 
@@ -30,6 +31,7 @@ for count in range (Number):
                 c+=1
             flag = "YES"
         except:
+            #wrong ticker entered
             print("WRONG TICKER")
             coin=input("RE-ENTER COIN: ")
 
@@ -53,13 +55,16 @@ for count in range (Number):
           for side in ["bids", "asks"]}
 
     buy_price = frames["asks"].price.min()
+    #sets the buy price below the last traded price so that the user will be a market maker and not market taker, hence need to pay less fee on the transaction
     buy_price = round((buy_price-(10*float(data["symbols"][c]["filters"][0]["tickSize"]))),dp1)
-    qty = round_up((float(40)/buy_price),dp2)#buy each coin for $40
+    #buy each coin for $40
+    qty = round_up((float(40)/buy_price),dp2)
     df.loc[count, 'COIN'] = coin
     df.loc[count, 'DP1'] = dp1
     df.loc[count, 'DP2'] = dp2
     df.to_csv("data.csv", index=False)
 
+    #places the buy limit order for it to be filled.
     buy_limit  = client.order_limit_buy(symbol=coin, quantity=qty, price=buy_price)
 
 
